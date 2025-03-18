07f2ab8b66b8d60a60865074f09ad20066e1aa2d
Anish Jain <anishsinghvi4@gmail.com>
2025-03-18 12:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function manipulateArray() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+manipulateArray()
+    .then((array) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evenNumbers = array.filter(num => num % 2 === 0);
+                document.getElementById("output").textContent = evenNumbers.join(",");
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const doubledNumbers = evenNumbers.map(num => num * 2);
+                document.getElementById("output").textContent = doubledNumbers.join(",");
+                resolve(doubledNumbers);
+            }, 2000);
+        });
+    });
